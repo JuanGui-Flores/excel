03eb0d2987fa333bb7e9c0d0521217e1ee1d408b
JuanGui-Flores <juanguiflores2002@gmail.com>
2023-04-13 15:56:54 -0300

    probar

--- fechas.py
+++ fechas.py
@@ -4,19 +4,20 @@ def cambiar_formato_fecha(fecha):
     return fecha.strftime('%d-%m-%Y')
 
 def cambiar_valores(valor):
-    if valor == 'A':
-        return 'Funcionalidad Planificada'
-    elif valor == 'B':
-        return 'Tarea Planificada'
-    elif valor == 'C':
-        return 'Tarea No Planificada'
+    if valor == 'Funcionalidad Planificada':
+        return 'A'
+    elif valor == 'Tarea Planificada':
+        return 'B'
+    elif valor == 'Tarea No Planificada':
+        return 'C'
     else:
         return valor
 
-df = pd.read_excel("C:\Work\jira-search-0b651e7f-5ce9-4912-9e53-c9a4cbd42e77.xlsx")
+df = pd.read_excel(r"C:\Work\jira-search-0b651e7f-5ce9-4912-9e53-c9a4cbd42e77.xlsx")
+
 
 df['fecha'] = df['fecha'].apply(cambiar_formato_fecha)
 df['estado'] = df['estado'].apply(cambiar_valores)
 df['tipo_incidencia'] = df['tipo_incidencia'].apply(cambiar_valores)
 
-df.to_excel("C:\Work\jira-search-0b651e7f-5ce9-4912-9e53-c9a4cbd42e77.xlsx", index=False)
+df.to_excel(r"C:\Work\jira-search-0b651e7f-5ce9-4912-9e53-c9a4cbd42e77.xlsx", index=False)
