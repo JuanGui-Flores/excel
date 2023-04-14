1c6328546734c85805f04eed4b415331b91c6650
JuanGui-Flores <juanguiflores2002@gmail.com>
2023-04-14 11:45:51 -0300

    act

--- fechas.py
+++ fechas.py
@@ -1,39 +1,59 @@
-import pandas as pd
+import openpyxl
 
-def change_date_format(df, column_name):
-    # Cambia el formato de fecha en un dataframe Pandas a dd/mm/yyyy.
-    df[column_name] = pd.to_datetime(df[column_name]).dt.strftime('%d/%m/%Y')
-    return df
+# Configuración
+archivoExcel = "C:\Work\jira-search-0b651e7f-5ce9-4912-9e53-c9a4cbd42e77.xlsx"
+columnaFechaVencimiento = 'Fecha de vencimiento'
+columnaEstado = 'Estado'
+valorEstadoProduccion = 'PRODUCCION'
+valorEstadoCerrado = 'CERRADO'
+columnaTipoIncidencia = 'Tipo de Incidencia'
+tiposIncidencia = [
+    'Funcionalidad Planificada',
+    'Tarea Planificada',
+    'Tarea No Planificada'
+]
 
-def change_estado(df, column_name):
-    # Cambia los valores de Estado en un dataframe Pandas.
-    df[column_name] = df[column_name].apply(lambda x: 'Funcionalidad Planificada' if x == 'Planificada' else 'Funcionalidad No Planificada')
-    return df
+# Función para formatear la fecha
+def formatearFecha(fecha):
+    return fecha.strftime('%d%m%Y')
 
-def change_tipo_incidencia(df, column_name):
-    # Cambia los valores de Tipo de Incidencia en un dataframe Pandas.
-    df[column_name] = df[column_name].apply(lambda x: 'Tarea Planificada' if x == 'Tarea Planificada' else 'Tarea No Planificada')
-    return df
-
-def save_dataframe_to_excel(df, excel_filename):
-    # Guarda un dataframe Pandas en un archivo Excel.
-    df.to_excel(excel_filename, index=False)
-
-def main():
-    # Leer el archivo Excel
-    df = pd.read_excel("C:/Work/jira-search-0b651e7f-5ce9-4912-9e53-c9a4cbd42e77.xlsx")
-
+# Función para actualizar una fila de datos
+def actualizarFila(fila):
     # Cambiar el formato de la fecha
-    df = change_date_format(df, 'Fecha')
+    fechaActualizada = fila[worksheet[columnaFechaVencimiento].column_letter + str(fila.row)].value
+    fila[worksheet[columnaFechaVencimiento].column_letter + str(fila.row)].value = formatearFecha(fechaActualizada)
 
-    # Cambiar los valores de Estado
-    df = change_estado(df, 'Estado')
+    # Cambiar el estado de la tarea
+    estado = fila[worksheet[columnaEstado].column_letter + str(fila.row)].value
+    if estado == valorEstadoProduccion:
+        fila[worksheet[columnaEstado].column_letter + str(fila.row)].value = valorEstadoCerrado
 
-    # Cambiar los valores de Tipo de Incidencia
-    df = change_tipo_incidencia(df, 'Tipo de Incidencia')
+    # Cambiar el tipo de incidencia si es válido
+    tipoIncidencia = fila[worksheet[columnaTipoIncidencia].column_letter + str(fila.row)].value
+    if tipoIncidencia not in tiposIncidencia:
+        print(f"Error: Tipo de incidencia no válida para la fila {fila.row}")
+    else:
+        fila[worksheet[columnaTipoIncidencia].column_letter + str(fila.row)].value = tipoIncidencia
 
-    # Guardar el archivo Excel modificado
-    save_dataframe_to_excel(df, "C:/Work/jira-search-0b651e7f-5ce9-4912-9e53-c9a4cbd42e77.xlsx")
+    return fila
 
-if __name__ == '__main__':
-    main()
+try:
+    # Leer el archivo Excel
+    workbook = openpyxl.load_workbook(archivoExcel)
+
+    # Seleccionar la hoja de trabajo
+    sheetName = workbook.sheetnames[0]
+    worksheet = workbook[sheetName]
+
+    # Iterar sobre las filas y hacer los cambios necesarios
+    for row in worksheet.iter_rows(min_row=2):
+        actualizarFila(row)
+
+    # Guardar el archivo Excel actualizado
+    newArchivoExcel = archivoExcel.replace('.xlsx', "C:\Work\jira-search-0b651e7f-5ce9-4912-9e53-c9a4cbd42e77.xlsx")
+    workbook.save(newArchivoExcel)
+
+    print(f"Archivo {newArchivoExcel} actualizado con éxito!")
+
+except Exception as e:
+    print(f"Error al actualizar el archivo Excel: {str(e)}")
