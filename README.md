44a79480d7ebdc66af5135d11296adcabbbcd020
JuanGui-Flores <juanguiflores2002@gmail.com>
2023-04-19 14:16:33 -0300

    a

---
+++ README.md
@@ -0,0 +1,12 @@
+NOMBRE DEL PROYECTO
+script excel
+
+REQUISITOS
+Python 3.x
+Lista de otras dependencias necesarias
+
+INSTALACIÓN
+Clonar este repositorio: git clone https://github.com/JuanGui-Flores/excel.git
+Ir al directorio del proyecto: cd repo
+Instalar las dependencias
+Ejecutar la aplicación: python app.py
--- formatos.py
+++ formatos.py
@@ -1,79 +1,68 @@
-import argparse
 import openpyxl
 from datetime import datetime
 
-# Definir argumentos de línea de comandoss
-parser = argparse.ArgumentParser(description='Actualizar archivo Excel')
-parser.add_argument('-i', '--input', type=str, help='Archivo de entrada (formato .xlsx)', required=True)
-parser.add_argument('-o', '--output', type=str, help='Archivo de salida (formato .csv)', required=True)
-args = parser.parse_args()
+def actualizar_archivo_excel(archivo_excel, archivo_csv):
+    # Configuración
+    COLUMNAS = {
+        'fecha_vencimiento': 'Fecha de vencimiento',
+        'estado': 'Estado',
+        'tipo_incidencia': 'Tipo de Incidencia',
+    }
+    ESTADOS = {
+        'PRODUCCION': 'CERRADO',
+        'CERRADO': 'CERRADO',
+    }
+    TIPOS_INCIDENCIA_VALIDOS = [
+        'Funcionalidad Planificada',
+        'Tarea Planificada',
+        'Tarea No Planificada'
+    ]
 
-# Configuración
-archivoExcel = args.input
-archivoCSV = args.output
-columnaFechaVencimiento = 'Fecha de vencimiento'
-columnaEstado = 'Estado'
-valorEstadoProduccion = 'PRODUCCION'
-valorEstadoCerrado = 'CERRADO'
-columnaTipoIncidencia = 'Tipo de Incidencia'
-tiposIncidencia = [
-    'Funcionalidad Planificada',
-    'Tarea Planificada',
-    'Tarea No Planificada'
-]
+    # Función para formatear la fecha
+    def formatear_fecha(fecha):
+        if isinstance(fecha, datetime):
+            return fecha.strftime('%d%m%Y')
+        return fecha
 
-# Función para formatear la fecha
-def formatearFecha(fecha):
-    return fecha.strftime('%d%m%Y')
+    # Función para formatear el estado
+    def formatear_estado(estado):
+        return ESTADOS.get(estado, estado)
 
-# Función para formatear el estado
-def formatearEstado(estado):
-    if estado == valorEstadoProduccion:
-        return valorEstadoCerrado
-    return estado
+    # Función para formatear el tipo de incidencia
+    def formatear_tipo_incidencia(tipo_incidencia):
+        if tipo_incidencia not in TIPOS_INCIDENCIA_VALIDOS:
+            raise ValueError(f'Tipo de incidencia no válida: {tipo_incidencia}')
+        return tipo_incidencia
 
-# Función para formatear el tipo de incidencia
-def formatearTipoIncidencia(tipoIncidencia):
-    if tipoIncidencia not in tiposIncidencia:
-        raise ValueError(f'Tipo de incidencia no válida: {tipoIncidencia}')
-    return tipoIncidencia
+    try:
+        # Leer el archivo Excel
+        workbook = openpyxl.load_workbook(archivo_excel)
 
-# Función para actualizar una fila de datos
-def actualizarFila(fila):
+        # Seleccionar la hoja de trabajo
+        worksheet = workbook.active
 
-    # Cambiar el formato de la fecha
-    fechaActualizada = fila[worksheet[columnaFechaVencimiento].column_letter + str(fila.row)].value
-    if isinstance(fechaActualizada, datetime):
-        fila[worksheet[columnaFechaVencimiento].column_letter + str(fila.row)].value = formatearFecha(fechaActualizada)
+        # Iterar sobre las filas y hacer los cambios necesarios
+        for row in worksheet.iter_rows(min_row=2):
+            # Cambiar el formato de la fecha
+            fecha_actualizada = row[COLUMNAS['fecha_vencimiento']].value
+            row[COLUMNAS['fecha_vencimiento']].value = formatear_fecha(fecha_actualizada)
 
-    # Cambiar el estado de la tarea
-    estado = fila[worksheet[columnaEstado].column_letter + str(fila.row)].value
-    fila[worksheet[columnaEstado].column_letter + str(fila.row)].value = formatearEstado(estado)
+            # Cambiar el estado de la tarea
+            estado = row[COLUMNAS['estado']].value
+            row[COLUMNAS['estado']].value = formatear_estado(estado)
 
-    # Cambiar el tipo de incidencia si es válido
-    tipoIncidencia = fila[worksheet[columnaTipoIncidencia].column_letter + str(fila.row)].value
-    if tipoIncidencia:
-        fila[worksheet[columnaTipoIncidencia].column_letter + str(fila.row)].value = formatearTipoIncidencia(tipoIncidencia)
+            # Cambiar el tipo de incidencia si es válido
+            tipo_incidencia = row[COLUMNAS['tipo_incidencia']].value
+            if tipo_incidencia:
+                row[COLUMNAS['tipo_incidencia']].value = formatear_tipo_incidencia(tipo_incidencia)
 
-    return fila
+        # Guardar el archivo Excel actualizado
+        workbook.save(archivo_csv)
+        workbook.close()
 
-try:
-    # Leer el archivo Excel
-    workbook = openpyxl.load_workbook(archivoExcel)
+        print(f"Archivo {archivo_csv} actualizado con éxito!")
 
-    # Seleccionar la hoja de trabajo
-    sheetName = workbook.sheetnames[0]
-    worksheet = workbook[sheetName]
-
-    # Iterar sobre las filas y hacer los cambios necesarios
-    for row in worksheet.iter_rows(min_row=2):
-        actualizarFila(row)
-
-    # Guardar el archivo Excel actualizado
-    workbook.save(archivoCSV)
-    print(f"Archivo {archivoCSV} actualizado con éxito!")
-
-except FileNotFoundError:
-    print(f"Error: El archivo {archivoExcel} no existe.")
-except Exception as e:
-    print(f"Error al actualizar el archivo: {e}")
+    except FileNotFoundError:
+        print(f"Error: El archivo {archivo_excel} no existe.")
+    except Exception as e:
+        print(f"Error al actualizar el archivo: {e}")
