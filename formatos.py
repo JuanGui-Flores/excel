ac8ac2114fff3f04f7a4e11ee8c1a3fc64691ce3
JuanGui-Flores <juanguiflores2002@gmail.com>
2023-05-02 15:05:08 -0300

    a

--- formatos.py
+++ formatos.py
@@ -1,6 +1,6 @@
+import csv
 import openpyxl
 from datetime import datetime
-
 
 def actualizar_archivo_excel(archivo_excel, archivo_csv):
     # Configuración
@@ -38,35 +38,41 @@ def actualizar_archivo_excel(archivo_excel, archivo_csv):
 
     try:
         # Leer el archivo Excel
-        workbook = openpyxl.load_workbook("C:\Work\jira.xlsx")
+        workbook = openpyxl.load_workbook(archivo_excel)
 
         # Seleccionar la hoja de trabajo
         worksheet = workbook.active
 
-        # Iterar sobre las filas y hacer los cambios necesarios
-        for row in worksheet.iter_rows(min_row=2):
-            # Cambiar el formato de la fecha
-            fecha_actualizada = row[COLUMNAS['fecha_vencimiento']].value
-            row[COLUMNAS['fecha_vencimiento']
-                ].value = formatear_fecha(fecha_actualizada)
+        # Crear archivo CSV
+        with open(archivo_csv, mode='w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
 
-            # Cambiar el estado de la tarea
-            estado = row[COLUMNAS['estado']].value
-            row[COLUMNAS['estado']].value = formatear_estado(estado)
+            # Escribir cabecera
+            header = [COLUMNAS[columna] for columna in COLUMNAS]
+            writer.writerow(header)
 
-            # Cambiar el tipo de incidencia si es válido
-            tipo_incidencia = row[COLUMNAS['tipo_incidencia']].value
-            if tipo_incidencia:
-                row[COLUMNAS['tipo_incidencia']
-                    ].value = formatear_tipo_incidencia(tipo_incidencia)
+            # Iterar sobre las filas y hacer los cambios necesarios
+            for row in worksheet.iter_rows(min_row=2):
+                # Cambiar el formato de la fecha
+                fecha_actualizada = row[COLUMNAS['fecha_vencimiento']].value
+                row[COLUMNAS['fecha_vencimiento']].value = formatear_fecha(fecha_actualizada)
 
-        # Guardar el archivo Excel actualizado
-        workbook.save(archivo_csv)
+                # Cambiar el estado de la tarea
+                estado = row[COLUMNAS['estado']].value
+                row[COLUMNAS['estado']].value = formatear_estado(estado)
+
+                # Cambiar el tipo de incidencia si es válido
+                tipo_incidencia = row[COLUMNAS['tipo_incidencia']].value
+                if tipo_incidencia:
+                    row[COLUMNAS['tipo_incidencia']].value = formatear_tipo_incidencia(tipo_incidencia)
+
+                # Escribir fila actualizada en archivo CSV
+                writer.writerow([row[COLUMNAS[columna]].value for columna in COLUMNAS])
+
+        workbook.save()
         workbook.close()
 
         print(f"Archivo {archivo_csv} actualizado con éxito!")
 
     except FileNotFoundError:
         print(f"Error: El archivo {archivo_excel} no existe.")
-    except Exception as e:
-        print(f"Error al actualizar el archivo: {e}")
